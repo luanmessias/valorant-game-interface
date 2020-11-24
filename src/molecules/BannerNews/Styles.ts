c4e9d5dd3dfc6f4700e424594d5113ae1b924fd9
Luan Messias <luanmessias10@gmail.com>
2020-11-24 11:03:19 -0300

    Created banner component

---
+++ src/molecules/BannerNews/Styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-image: url('/img/banner-news.jpg');
+  background-color: #000;
+  width: 319px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  div {
+    padding: 20px;
+  }
+  strong {
+    font-size: 27px;
+    color: #fff;
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  span {
+    font-size: 14px;
+    font-weight: 300;
+    color: #fff;
+  }
+`;
---
+++ src/molecules/BannerNews/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './Styles';
+
+export default function BannerNews(): JSX.Element {
+  return (
+    <Container>
+      <div>
+        <strong>DESTAQUES</strong>
+        <span>NOVO PERSONAGEM</span>
+      </div>
+    </Container>
+  );
+}
