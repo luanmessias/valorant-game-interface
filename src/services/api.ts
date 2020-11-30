b34322c381cd710ceb6347f37c8dc38762c4646a
Luan Messias <luanmessias10@gmail.com>
2020-11-30 12:15:42 -0300

    Added online fake API Mock

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  // Local
+  // baseURL: 'http://localhost:3333'
+
+  // Online
+  baseURL: 'https://uiclone-valorant.free.beeceptor.com/'
 })
 
 export default api
