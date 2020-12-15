43b52064ba3426d5e48d8916e78ba34b27b5bb61
Luan Messias <luanmessias10@gmail.com>
2020-12-15 18:35:49 -0300

    Changed next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, './public/svg'),
+  images: {
+    domains: ['cdn.valorantinfo.gg'],
+  },
   webpack(config, options) {
     return config
   }
