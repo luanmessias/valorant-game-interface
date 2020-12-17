dd703188dde89ff3521d48bc28cc94d125b98ed1
Luan Messias <luanmessias10@gmail.com>
2020-12-17 09:23:35 -0300

    Changed next.config

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ const path = require('path')
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, './public/svg'),
-  images: {
-    domains: ['cdn.valorantinfo.gg'],
-  },
   webpack(config, options) {
     return config
   }
 });
-
-module.exports = {
-  exportPathMap: async function() {
-    return {
-      "/": { page: "/" },
-      "/carrier": { page: "/carrier" },
-      "/collection": { page: "/collection" },
-      "/store": { page: "/store" },
-    };
-  }
-};
