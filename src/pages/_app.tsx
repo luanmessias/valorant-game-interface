b4f2db9ef2e2831cd8d720f01ca97c54f0f74761
Luan Messias <luanmessias10@gmail.com>
2020-12-17 14:19:06 -0300

    Add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import MainNav from '../molecules/MainNav'
 import { ThemeProvider } from 'styled-components'
@@ -8,8 +10,17 @@ import { ModalProvider } from '@/context/Modal'
 import Modal from '@/molecules/Modal'
 import { BGVideoProvider } from '@/context/BackgroundVideo'
 import { AnimatePresence } from 'framer-motion'
+import '../styles/global/nprogress.css'
 import GlobalNormalize from '../styles/global/normalize'
 import GlobalStyles, { Main, BGImage, Wrapper } from '../styles/global/global'
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routechangeError', () => NProgress.done())
 
 export default function MyApp({
   Component,
