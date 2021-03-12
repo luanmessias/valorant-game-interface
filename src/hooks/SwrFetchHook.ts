16ecbe8bcefc6d2b45616649e7f0bd5ccf04370a
Luan Messias <luanmessias10@gmail.com>
2021-03-12 08:15:47 -0300

    Changed swr fetch method to test api on server

--- src/hooks/SwrFetchHook.ts
+++ src/hooks/SwrFetchHook.ts
@@ -1,16 +1,17 @@
 import api from 'src/services/api'
 import useSWR from 'swr'
 
-const fetcher = url => fetch(url).then(res => res.json())
 function SwrFetchHook<Data = any, Error = any>(url: string) {
   const { data, error } = useSWR<Data, Error>(url, async url => {
-    /* Fetch method: */
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
+    /* Fetch method:
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    */
+
     /* Axios method (require import the API) */
-    // const response = await api.get(url)
-    // return response.data
+    const response = await api.get(url)
+    return response.data
   })
 
   return { data, error }
