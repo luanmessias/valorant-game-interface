8e0bef8d8620a54575aec52e3c93def11ca25b04
Luan Messias <luanmessias10@gmail.com>
2021-03-12 00:01:48 -0300

    Changed json server to cors

--- src/pages/api/[endpoint].ts
+++ src/pages/api/[endpoint].ts
@@ -1,16 +1,7 @@
-import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
+import { VercelRequest, VercelResponse } from '@vercel/node'
 import data from './data.json'
 
-const cors = initMiddleware(
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET']
-  })
-)
-
-export default async function handler(req, res) {
-  const { endpoint } = req.query
-  await cors(req, res)
-  res.json(data[endpoint])
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { endpoint } = request.query
+  response.json(data[endpoint])
 }
