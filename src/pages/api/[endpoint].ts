314f55faaa0fbb4c1e4165de55139db1c6205daf
Luan Messias <luanmessias10@gmail.com>
2021-03-12 00:03:20 -0300

    Changed json server to cors with node

--- src/pages/api/[endpoint].ts
+++ src/pages/api/[endpoint].ts
@@ -3,5 +3,5 @@ import data from './data.json'
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { endpoint } = request.query
-  response.json(data[endpoint])
+  response.json(data[`${endpoint}`])
 }
