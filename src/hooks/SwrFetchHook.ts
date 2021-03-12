8ae9520fc4f6ba2716b9887895abd809ef79b742
Luan Messias <luanmessias10@gmail.com>
2021-03-12 07:50:08 -0300

    Changed json server to pages

--- src/hooks/SwrFetchHook.ts
+++ src/hooks/SwrFetchHook.ts
@@ -3,15 +3,13 @@ import useSWR from 'swr'
 
 function SwrFetchHook<Data = any, Error = any>(url: string) {
   const { data, error } = useSWR<Data, Error>(url, async url => {
-    /* Fetch method:
-        const response = await fetch(url)
-        const data = await response.json()
-        return data
-    */
-
+    /* Fetch method: */
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
     /* Axios method (require import the API) */
-    const response = await api.get(url)
-    return response.data
+    // const response = await api.get(url)
+    // return response.data
   })
 
   return { data, error }
