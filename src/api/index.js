51eab48269b21d1657db818e5e3b044160b64097
Luan Messias <luanmessias10@gmail.com>
2021-03-12 00:31:30 -0300

    Changed json server

--- src/api/index.js
+++
@@ -1,5 +0,0 @@
-import data from './data.json'
-
-export default function handler(req, res) {
-  res.status(200).json(data)
-}
--- src/pages/api/[endpoint].ts
+++
@@ -1,7 +0,0 @@
-import data from './data.json'
-
-export default function dataHandler(req, res) {
-  const { endpoint } = req.query
-
-  res.status(200).json(data[endpoint])
-}
