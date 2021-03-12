d654d5c46d0bcff3f16924b0eba94a2644482d64
Luan Messias <luanmessias10@gmail.com>
2021-03-11 23:32:12 -0300

    Changed json server to cors

--- api/index.tsx
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import data from './data.json'
-
-export default (_req: VercelRequest, res: VercelResponse) => {
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Allow-Origin', '*')
-
-  return res.json({ message: 'Hello World' })
-}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://valorant-game-interface.vercel.app/api/'
+  baseURL: '/api/'
 })
 
 export default api
