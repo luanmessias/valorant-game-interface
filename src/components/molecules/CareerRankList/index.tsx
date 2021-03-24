15373e3c5c92513a6e51d4af5d160c7dd3c90ed5
Luan Messias <luanmessias10@gmail.com>
2021-03-24 20:44:13 -0300

    Add data and styles to leaderboard table

--- src/components/molecules/CareerRankList/index.tsx
+++ src/components/molecules/CareerRankList/index.tsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import SwrFetchHook from '@/hooks/SwrFetchHook'
 import PlaceholderTag from '@/atoms/PlaceholderTag'
-import { Container, HeadList, TitleHead } from './styles'
+import Image from 'next/image'
+import {
+  Container,
+  HeadList,
+  TitleHead,
+  LeaderboardContainer,
+  RankList,
+  RankItem,
+  RankItemPosition,
+  RankItemRating,
+  RankItemDetails,
+  RankItemWon
+} from './styles'
 
 interface Leaderboard {
   name: string
@@ -26,13 +38,18 @@ interface Ranking {
   img: string
 }
 
+interface Card {
+  id: number
+  url: string
+}
+
 const CareerRankList = () => {
-  const [data, setData] = useState([])
   const careerData = SwrFetchHook<Leaderboard[]>('/api/leaderboard').data
   const playerData = SwrFetchHook<Friend[]>('/api/friends').data
   const rankingData = SwrFetchHook<Ranking[]>('/api/ranking').data
+  const cardsData = SwrFetchHook<Card[]>('/api/cards').data
 
-  if (!careerData || !playerData || !rankingData) {
+  if (!careerData || !playerData || !rankingData || !cardsData) {
     return (
       <Container>
         <PlaceholderTag
@@ -45,15 +62,87 @@ const CareerRankList = () => {
     )
   }
 
+  const playerWithCards = playerData.map(player => ({
+    ...player,
+    cardDetail: cardsData.find(card => card.id === player.card)
+  }))
+
+  const fullData = careerData.map(career => ({
+    ...career,
+    playerDetail: playerWithCards.find(player => player.name === career.name),
+    rankingDetail: rankingData.find(rank => rank.id === career.rank)
+  }))
+
+  if (!fullData) {
+    return (
+      <Container>
+        <PlaceholderTag
+          width="100%"
+          height="300px"
+          color="transparent"
+          showLoading={true}
+        />
+      </Container>
+    )
+  }
+
+  console.log(fullData)
+
   return (
     <Container>
-      <HeadList>
-        <TitleHead>RANK</TitleHead>
-        <TitleHead>RATING</TitleHead>
-        <TitleHead></TitleHead>
-        <TitleHead></TitleHead>
-        <TitleHead>Ended on 3/2</TitleHead>
-      </HeadList>
+      <LeaderboardContainer>
+        <HeadList>
+          <TitleHead>RANK</TitleHead>
+          <TitleHead>RATING</TitleHead>
+          <TitleHead></TitleHead>
+          <TitleHead>Ended on 3/2</TitleHead>
+        </HeadList>
+        <RankList>
+          {fullData.map(fd => {
+            const {
+              name,
+              playerDetail,
+              position,
+              rankingDetail,
+              rating,
+              team,
+              won
+            } = fd
+
+            return (
+              <RankItem key={position} data-first={position === 1}>
+                <RankItemPosition>
+                  <strong>{position}</strong>
+                </RankItemPosition>
+                <RankItemRating>
+                  <Image
+                    src={`/img/rank-icons/${rankingDetail.img}`}
+                    width={45}
+                    height={45}
+                  />
+                  <span>{rating}</span>
+                </RankItemRating>
+                <RankItemDetails>
+                  <Image
+                    src={`/img/profile-avatars/${playerDetail.avatar}.png`}
+                    width={45}
+                    height={45}
+                  />
+                  <div className="playerInfo">
+                    <strong>{name}</strong>
+                    <span>{team}</span>
+                  </div>
+                </RankItemDetails>
+                <RankItemWon>
+                  <div>
+                    <span>{won}</span> GAMES WON
+                  </div>
+                </RankItemWon>
+              </RankItem>
+            )
+          })}
+        </RankList>
+      </LeaderboardContainer>
     </Container>
   )
 }
--- src/components/molecules/CareerRankList/styles.ts
+++ src/components/molecules/CareerRankList/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
-const ColRankWidth = '20%'
-const ColRatingWidth = '10%'
+const ColRankWidth = '10%'
+const ColRatingWidth = '20%'
 const ColProfileWidth = '50%'
 const ColGamesWidth = '20%'
 
@@ -13,6 +13,8 @@ export const Container = styled.div`
   background-color: rgba(255, 255, 255, 0.1);
 `
 
+export const LeaderboardContainer = styled.div``
+
 export const HeadList = styled.div`
   display: flex;
   flex-direction: row;
@@ -23,8 +25,10 @@ export const HeadList = styled.div`
 `
 
 export const TitleHead = styled.div`
-  text-align: center;
+  display: flex;
   color: #b9cdf1;
+  justify-content: center;
+  align-items: center;
 
   &:nth-of-type(1) {
     width: ${ColRankWidth};
@@ -36,9 +40,89 @@ export const TitleHead = styled.div`
     width: ${ColProfileWidth};
   }
   &:nth-of-type(4) {
-    width: ${ColProfileWidth};
-  }
-  &:nth-of-type(5) {
     width: ${ColGamesWidth};
+    justify-content: flex-end;
+    padding-right: 15px;
+  }
+`
+
+export const RankList = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+`
+export const RankItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-bottom: 1px solid #5a657a;
+
+  > div {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+  }
+`
+export const RankItemPosition = styled.div`
+  width: ${ColRankWidth};
+  justify-content: center;
+  align-items: center;
+  font-size: 35px;
+  font-weight: 300;
+  color: #fff;
+`
+export const RankItemRating = styled.div`
+  width: ${ColRatingWidth};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.05);
+  span {
+    font-size: 16px;
+    color: #fff;
+    margin-left: 40px;
+  }
+`
+export const RankItemDetails = styled.div`
+  width: ${ColProfileWidth};
+  background-color: rgba(255, 255, 255, 0.05);
+
+  .playerInfo {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-around;
+    height: 45px;
+    margin-left: 20px;
+
+    strong {
+      font-size: 18px;
+      color: #fff;
+    }
+
+    span {
+      font-size: 12px;
+      color: #c2c2c2;
+    }
+  }
+`
+export const RankItemWon = styled.div`
+  width: ${ColGamesWidth};
+  background-color: rgba(255, 255, 255, 0.05);
+  justify-content: flex-end;
+  padding-right: 15px;
+
+  div {
+    font-size: 14px;
+    color: #c2c2c2;
+    span {
+      opacity: 1;
+      color: #fff;
+    }
   }
 `
